3aa3cb347901fc65ef101248ed6af71ebef3a502
Elizabeth Engelman <emengelman@gmail.com>
2014-06-22 17:39:17 -0500

    creates a new amount_to_charge function

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Customer
   attr_reader :name
 
@@ -13,20 +15,9 @@ class Customer
   def statement
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
+    this_amount = 0
     @rentals.each do |rental|
-      this_amount = 0
-
-      # determine amounts for each line
-      case rental.movie.price_code
-      when Movie::REGULAR
-        this_amount += 2
-        this_amount += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-      when Movie::NEW_RELEASE
-        this_amount += rental.days_rented * 3
-      when Movie::CHILDRENS
-        this_amount += 2
-        this_amount += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-      end
+      this_amount += amount_to_charge(rental)
 
       #add frequent renter points
       frequent_renter_points += 1
@@ -42,5 +33,21 @@ class Customer
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
+  end
+
+  def amount_to_charge(rental)
+    # determine amounts for each line
+    this_amount = 0
+    case rental.movie.price_code
+    when Movie::REGULAR
+      this_amount += 2
+      this_amount += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
+    when Movie::NEW_RELEASE
+      this_amount += rental.days_rented * 3
+    when Movie::CHILDRENS
+      this_amount += 2
+      this_amount += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
+    end
+    this_amount
   end
 end
