80d61d727715ec323f4e58d689b0750c48121d26
Elizabeth Engelman <emengelman@gmail.com>
2014-06-22 18:21:07 -0500

    removes the temporary variable total_amount and adds a new function to do this calc

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,20 +13,23 @@ class Customer
   end
 
   def statement
-    total_amount = 0
     frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |rental|
       rental_charge = rental.amount_to_charge
       frequent_renter_points = rental.get_frequent_renter_points
       result += "\t" + rental.movie.title + "\t" + rental_charge.to_s + "\n"
-      total_amount += rental_charge
     end
 
-    result += "Amount owed is #{total_amount}\n"
+    result += "Amount owed is #{get_total_amount(@rentals)}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
   end
 
+  def get_total_amount(rentals)
+    rentals.map(&:amount_to_charge).reduce(0) do |sum, amount|
+      sum + amount
+    end
+  end
 
 end
