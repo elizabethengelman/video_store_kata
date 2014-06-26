bb907d622e1518e59393024e23d958e1f3859d0e
Elizabeth Engelman <emengelman@gmail.com>
2014-06-26 18:29:58 -0500

    starting with get_charge and get_freq_pts already in Movie class

--- lib/movie.rb
+++ lib/movie.rb
@@ -10,4 +10,28 @@ class Movie
     @title = title
     @price_code = price_code
   end
+
+  def get_charge(days_rented)
+    this_amount = 0
+    case price_code
+    when REGULAR
+      this_amount += 2
+      this_amount += (days_rented - 2) * 1.5 if days_rented > 2
+    when NEW_RELEASE
+      this_amount += days_rented * 3
+    when CHILDRENS
+      this_amount += 2
+      this_amount += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    this_amount
+  end
+
+  def get_frequent_renter_points(days_rented)
+    frequent_renter_points = 1
+    # add bonus for a two day new release rental
+    if @price_code == NEW_RELEASE && days_rented > 1
+      frequent_renter_points += 1
+    end
+    frequent_renter_points
+  end
 end
